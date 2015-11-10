41f34d4dca4980c7c03bd1e007a6ca65c2d6fdcd
Matt Fair <matt.j.fair@gmail.com>
2015-11-10 13:19:23 -0500

    updated so that editing and deleting posts are only seen by user if they created it.  Also validated web address in model vs controller.  This closes issue #9

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,16 @@
 class Vote < ActiveRecord::Base
 
   validates_presence_of :link_id
+  validates :link_id, uniqueness: {scope: :user_id, message:"You can only vote once!"}
+
+  before_validation :confirm_web_address
+
+
+  def confirm_web_address
+    unless self.link.location[/\Ahttp:\/\//] || self.link.location[/\Ahttps:\/\//]
+    self.link.location = "http://"+self.link.location
+    end
+  end
 
   belongs_to :link
   belongs_to :user
